add579537dff87a7b400bf023a4f10a913e66ed4
christhiangoncalves <christhian.sg@hotmail.com>
2021-12-17 23:58:33 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,27 @@
+package com.dio;
+
+import com.dio.model.Gato; 
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livro livro = new Livro();
+		
+		System.out.println(gato);
+		System.out.println(livro);
+		/*int a = 2;
+		int b = 3;
+		
+		// TODO Auto-generated method stub
+		System.out.println("Hello Word "+(a+b));*/
+		
+		
+	}
+
+}
+class Livro{
+	private String nome;
+	private String npag;
+}
